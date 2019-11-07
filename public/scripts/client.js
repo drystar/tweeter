bcd77f99d9df87b387883cf4674b2de4f678f7cd
drystar <chris.drysdale12@gmail.com>
2019-11-07 20:38:50 +0000

    class & append tweet elements in order to generate new tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -35,4 +35,78 @@ const data = [
     "created_at": 1461113959088
   }
 ]
+// class each element of the tweet
+// appends elements to the article to generaate new tweet
 
+const createTweetElement = function (tweet) {
+
+  const $newTweet = $("<article>").addClass("new-tweet");
+
+  const $img = $("img").attr("src", tweet.user.avatars);
+
+  const $name = $("<h4>").addClass("name").text(tweet.user.name);
+
+  const $handle = $("<p>").addClass("handle").text(tweet.user.handle);
+
+  const $content = $("<div>").addClass("content").text(tweet.content.text);
+
+  const $timeStamp = $("<p>").addClass("timeStamp").text(tweet.created_at);
+
+  const $tweetIcons = $("<p>").addClass("icons").html('<i class="fa fa-flag-o"></i><i class="fas fa-retweet"></i><i class="fa fa-heart-o"></i>');
+
+
+    const $header = $("<header>").append($img).append($name).append($handle)
+    const $footer = $("<footer>").append($timeStamp).append($tweetIcons)
+
+return $newTweet.append($header).append($content).append($footer)
+
+ };
+
+
+//   // DRIVER FOR TEST CODE
+ $(document).ready(function (){
+ const $tweet = createTweetElement(data[0]);
+ console.log($tweet);
+ $('.all-tweets').append($tweet)
+});
+
+
+
+
+
+// === Failed Function Breakdown ===
+ // const $img = $('<img>').attr('src', tweet.user.avatars);
+
+ // const $header = $('header').addClass('header')
+
+ // const $article = $('<article>').addClass('tweet')
+
+ // const $tweet = $("<article>").addClass("tweet");
+ // const $header = $("<header>").addClass("tweet-header");
+
+ // const $img = $("<img>").addClass("tweet-img");
+ // const $name = $("<span>").addClass("tweet-name");
+ // const $handle = $("<span>").addClass("tweet-handle");
+
+ // const $body = $("<p>").addClass("tweet-body");
+
+ // const $footer = $("footer").addClass("tweet-footer");
+ // const $created_at = $("<span>").addClass("timeStamp");
+
+ // const $tweetIcons = $("<span>").addClass("tweet-icons");
+ // const $flag = $("<i>").addClass("font-icons");
+ // const $repeat = $("<i>").addClass("font-icons");
+ // const $favorite = $("<i>").addClass("font-icons");
+
+   // $name.append(tweet.user.name);
+   // $handle.append(tweet.user.handle);
+   // $header.append($img, $name, $handle);
+   // $body.append(tweet.content.text);
+   // $flag.append("flag");
+   // $repeat.append("retweet");
+   // $favorite.append("heart");
+   // $tweetIcons.append($flag, $repeat, $favorite);
+   // $created_at.append(tweet.createdAt);
+
+ // const $theTweet = $($tweet).append($header).append($body).append($footer);
+   // return $theTweet;
