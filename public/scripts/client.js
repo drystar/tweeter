25f854333212576fabe1b2ccda02f6e5459428ff
drystar <chris.drysdale12@gmail.com>
2019-11-09 09:31:44 +0000

    clean up || clear form/ reset char count initial function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,47 +1,9 @@
-// const data = [
-//   {
-//    "user": {
-//    "name": "Newton",
-//    "avatars": "https://i.imgur.com/73hZDYK.png",
-//    "handle": "@SirIsaac"
-//     },
-//     "content": {
-//      "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-    
-//   {
-//    "user": {
-//     "name": "Descartes",
-//     "avatars": "https://i.imgur.com/nlhLi3I.png",
-//     "handle": "@rd"
-//    },
-//      "content": {
-//       "text": "Je pense , donc je suis"
-//      },
-//     "created_at": 1461116232227
-//   },
 
-//   {
-//    "user": {
-//     "name": "Chris",
-//     "avatars": "https://i.imgur.com/nlhLi3I.png",
-//     "handle": "@chris" 
-//    },
-//     "content": {
-//       "text": "I am test. But I could be an additional object!"
-//       },
-//     "created_at": 1461113959088
-//   }
-// ]
-// class each element of the tweet
-// appends elements to the article to generaate new tweet
-
-const createTweetElement = function (tweet) {
+// create a new tweet article from user input
+  const createTweetElement = function (tweet) {
 
   const $newTweet = $("<article>").addClass("tweet");
-console.log("ppp", tweet)
+
   const $img = $("<img>").attr("src", tweet.user.avatars);
 
   const $name = $("<h4>").addClass("name").text(tweet.user.name);
@@ -58,22 +20,16 @@ console.log("ppp", tweet)
     const $header = $("<header>").append($img).append($name).append($handle)
     const $footer = $("<footer>").append($timeStamp).append($tweetIcons)
 
-return $newTweet.append($header).append($content).append($footer)
+  return $newTweet.append($header).append($content).append($footer)
 
  };
 
+ //jQuery loaded 
+ $(document).ready(function (){
 
-// //   // DRIVER FOR TEST CODE
-//  $(document).ready(function (){
-//  const $tweet = createTweetElement(data);
-//  console.log($tweet);
-//  $('.all-tweets').append($tweet)
-// });
+// function to take in an array of tweets and append each new tweet "all-tweets' container of tweeter page 
 
-// Note to self: comment out driver code to avoid excess running of createTweet function//bug
 
-// function takes an array of tweets and appends each new tweet to tweeter page 
-$(document).ready(function (){
    
 // function to toggle the new tweet
 $('#compose-new button').click(function () {
@@ -83,95 +39,60 @@ $('#compose-new button').click(function () {
   
   loadTweets();
 
+// screens for invalid tweets
+
 $('.new-tweet form').submit( function (event) {
 
-  const tweet = $('textarea').val();
+  const event = $('textarea').val();
 
   event.preventDefault();
   const $form = $(this);
   const $newTweetP = $form.children('textarea').val();
-  // const data = $form.serialize();
-  //console.log("lll", data)
-
+ 
 if ($newTweetP.length === 0) {
-  //return alert('Nothing to Say???????');
-  $('.new-tweet p').append('Nothing to Say???????')
-  $('.new-tweet p').slideDown();
+  
+  $('.new-tweet p').append('⚠️⚠️ Nothing to Say????? ⚠️⚠️')
+  $('.new-tweet p').slideDown("slow");
 
 }else if ($newTweetP.length > 140) {
-  // return alert ('Yikes you have too much to say today...');
-  $('.new-tweet p').append('Yikes you have too much to say today...')
-  $('.new-tweet p').slideDown();
+  
+  $('.new-tweet p').append('🚫🚫 Yikes you have too much to say today... 🚫🚫')
+  $('.new-tweet p').slideDown("slow");
 } else {
 
-$('.new-tweet p').slideUp();
-const serializedForm = $(this).serialize();
+  $('.new-tweet p').slideUp("fast");
+  const serializedForm = $(this).serialize();
+    // data: serializedForm
+    // function(data) {
+    //   $('.all-tweets').prepend(createTweetElement(data))
+    //   $('textarea').val('');
+    //   $('.counter').text(140)
+    //   })
+    // });
 
 
-  // console.log("before ajax");
-  // $.post('/tweets', tweet)
-  // ajax({ url: "/tweets", method: 'POST', data: tweet })
+  //  
   $.ajax({ url: "/tweets", method: 'POST', data: serializedForm })
   .then(()=>{loadTweets()})
 }
 })
-  // renderTweets(data);
 
 })
 
+// takes array of tweets || calls createTweetElement for each tweet
 const renderTweets = function(dataArr) {
   $('.all-tweets').empty();
   for (const tweet of dataArr) {
-   // console.log("sup", tweet)
     const $tweet = createTweetElement(tweet);
     $('.all-tweets').prepend($tweet);
   }
 };
 
+// load tweets without refreshing the page 
 const loadTweets = function () {
-  //$.get('/tweets')
   $.ajax({ url: "/tweets", method: 'GET' })
   .then(function (dataArr) {
     renderTweets(dataArr);
   
-   // $('.all-tweets').prepend(dataArr);
   })
 }
-
-
-// === Failed Function Breakdown ===
- // const $img = $('<img>').attr('src', tweet.user.avatars);
-
- // const $header = $('header').addClass('header')
-
- // const $article = $('<article>').addClass('tweet')
-
- // const $tweet = $("<article>").addClass("tweet");
- // const $header = $("<header>").addClass("tweet-header");
-
- // const $img = $("<img>").addClass("tweet-img");
- // const $name = $("<span>").addClass("tweet-name");
- // const $handle = $("<span>").addClass("tweet-handle");
-
- // const $body = $("<p>").addClass("tweet-body");
-
- // const $footer = $("footer").addClass("tweet-footer");
- // const $created_at = $("<span>").addClass("timeStamp");
-
- // const $tweetIcons = $("<span>").addClass("tweet-icons");
- // const $flag = $("<i>").addClass("font-icons");
- // const $repeat = $("<i>").addClass("font-icons");
- // const $favorite = $("<i>").addClass("font-icons");
-
-   // $name.append(tweet.user.name);
-   // $handle.append(tweet.user.handle);
-   // $header.append($img, $name, $handle);
-   // $body.append(tweet.content.text);
-   // $flag.append("flag");
-   // $repeat.append("retweet");
-   // $favorite.append("heart");
-   // $tweetIcons.append($flag, $repeat, $favorite);
-   // $created_at.append(tweet.createdAt);
-
- // const $theTweet = $($tweet).append($header).append($body).append($footer);
-   // return $theTweet;
